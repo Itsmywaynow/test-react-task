903a411100f2b45da8fde7783095cf6d5a874030
David Kondzharia <dkondzharia@gmail.com>
2021-11-11 18:58:09 +0300

    initialize project

---
+++ src/components/UsersList.jsx
@@ -0,0 +1,14 @@
+import { List, ListItem } from '@mui/material'
+import React from 'react'
+
+const UsersList = () => {
+  return (
+    <List>
+      <ListItem>1</ListItem>
+      <ListItem>2</ListItem>
+      <ListItem>3</ListItem>
+    </List>
+  )
+}
+
+export default UsersList
